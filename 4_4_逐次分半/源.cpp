a2e96dbe1d5dbc3941f32d07f1b6daf9b9869ad4
唐辉 <tanghui555@live.com>
2017-12-08 19:30:31 +0800

    1

--- 4_4_逐次分半/源.cpp
+++ 4_4_逐次分半/源.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 #define Error 1e-12
 
-void scanf_data(double temp_a[]);
-void scanf_data(double temp_a[], int &n);
+void scanf_data(double temp_a[]);			//重载函数
+void scanf_data(double temp_a[], int &n);	//输入数据
 void successive_half(double temp_a[], int n);
 double f_x(double x);
 double* integral_f(double temp_a[], int n, double(*fun)(double));
@@ -27,34 +27,40 @@ void successive_half(double temp_a[], int n)
 	double T_2n = 0;
 	double S_2n = 0;
 	double H_n = 0;
+	double S_n = 0,temp_s_n;
 	double(*fun)(double) = f_x;
-
+	double times = 0, S_times = 0;
 	double *temp = NULL;
 
 	temp = integral_f(temp_a, n, fun);
 	T_2n = temp[0];
-	cout << "******分别是n, Tn,T2n,S2n******\n";
-	while (fabs(error) > Error)
+	cout << "\n\n\ttimes\tn\t\tT2n\t\tS2n\n";
+	while (fabs(T_2n - T_n) > Error)
 	{
+		times++;
 		T_n = T_2n;
+		S_n = S_2n;
 		temp = integral_f(temp_a, n, fun);
 
 		T_2n = 0.5*temp[0] + 0.5*temp[1];
-
 		S_2n = 4.0 / 3.0 * T_2n - T_n / 3.0;
 
-		error = T_2n - T_n;
-		//T_n = T_2n;
-		n = n * 2;
-		cout << "n = " << n << " ";
-		cout <<setprecision(14) << " "<<T_n <<" "<<T_2n<<" "<<S_2n<<endl;
-		//getchar();
+		cout << "\t" << times << "\t" << n;
+		cout << setprecision(14) << "\t" << T_2n << "\t";
+		if (fabs(S_2n - S_n) > Error)
+		{
+			S_times = times;
+			temp_s_n = S_2n;
+			cout << S_2n << endl;
+		}
+		else	cout << endl;
 
+		n = n * 2;
 	}
 
 	cout << "\n\n************结果为*********\n";
-	cout  << "T2n = " << T_2n << endl;
-	cout  << "S2n = " << S_2n << endl;
+	cout << "\tT_2n运行" << times << "次，结果为 = " << T_2n << endl;
+	cout << "\tS_2n运行" << S_times << "次，结果为 = " <<temp_s_n << endl;
 }
 
 double f_x(double x)
@@ -72,7 +78,7 @@ double* integral_f(double temp_a[], int n, double(*fun)(double))
 	double x = temp_a[0];				//梯形的上底的x
 	double x_1 = temp_a[0];				//梯形的下底的x
 	double fx = 0;						//梯形的上底
-	double fx_1 = 1.0 / (1 + x_1*x_1);	//梯形的下底
+	double fx_1 = (*fun)(x_1);			//梯形的下底
 
 	double fx_0_1 = 0;					//中点的函数值
 	double rec_area = 0;				//矩形的面积
@@ -86,13 +92,9 @@ double* integral_f(double temp_a[], int n, double(*fun)(double))
 		fx_1 = (*fun)(x_1);
 
 		sum += 0.5*piece_len*(fx + fx_1);
-		//cout << "x = " << 0.5*(x + x_1) << endl;
-		//getchar();
 		fx_0_1 = (*fun)(0.5*(x + x_1));
 		rec_area += fx_0_1 * piece_len;
 	}
-	//cout << "\n\n************结果为*********\n";
-	//cout << sum <<" rec = "<< rec_area << endl;
 
 	area[0] = sum;
 	area[1] = rec_area;
